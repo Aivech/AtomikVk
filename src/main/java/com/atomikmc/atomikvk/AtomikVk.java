599092c2ec4cd248bff688e0c5126fa1045360ea
Aivech <kbsauer1@gmail.com>
2019-12-17 17:06:48 -0500

    physical device enumeration

--- src/main/java/com/atomikmc/atomikvk/AtomikVk.java
+++ src/main/java/com/atomikmc/atomikvk/AtomikVk.java
@@ -9,8 +9,20 @@ import org.lwjgl.glfw.GLFWVulkan;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.vulkan.*;
 
+import static java.lang.Math.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFWVulkan.*;
 import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.vulkan.EXTDebugReport.*;
+import static org.lwjgl.vulkan.KHRDedicatedAllocation.*;
+import static org.lwjgl.vulkan.KHRGetMemoryRequirements2.*;
+import static org.lwjgl.vulkan.KHRGetPhysicalDeviceProperties2.*;
+import static org.lwjgl.vulkan.KHRSurface.*;
+import static org.lwjgl.vulkan.KHRSwapchain.*;
+import static org.lwjgl.vulkan.VK11.*;
 
+import java.nio.IntBuffer;
 import java.nio.LongBuffer;
 
 public class AtomikVk {
@@ -19,32 +31,32 @@ public class AtomikVk {
 
     public static void main(String[] args) {
 
-        if (!GLFW.glfwInit()) {
+        if (!glfwInit()) {
             throw new AssertionError("GLFW init failed");
         }
 
-        GLFW.glfwSetErrorCallback((error, description) -> {
+        glfwSetErrorCallback((error, description) -> {
             logger.error("GLFW ERROR: " + error);
             logger.error(GLFWErrorCallback.getDescription(description));
         });
 
-        if (!GLFWVulkan.glfwVulkanSupported()) {
-            GLFW.glfwTerminate();
+        if (!glfwVulkanSupported()) {
+            glfwTerminate();
             throw new AssertionError("No Vulkan");
         }
 
-        PointerBuffer vkRequiredExtensions = GLFWVulkan.glfwGetRequiredInstanceExtensions();
+        PointerBuffer vkRequiredExtensions = glfwGetRequiredInstanceExtensions();
         if (vkRequiredExtensions == null) {
-            GLFW.glfwTerminate();
+            glfwTerminate();
             throw new AssertionError("Missing list of required Vulkan extensions");
         }
 
-        GLFW.glfwWindowHint(GLFW.GLFW_CLIENT_API, GLFW.GLFW_NO_API);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-        long window = GLFW.glfwCreateWindow(640, 480, "AtomikVk", 0, 0);
+        long window = glfwCreateWindow(640, 480, "AtomikVk", 0, 0);
 
         if (window == 0) {
-            GLFW.glfwTerminate();
+            glfwTerminate();
             throw new AssertionError("No window!");
         }
 
@@ -52,34 +64,39 @@ public class AtomikVk {
 
             PointerBuffer extensions = stack.mallocPointer(vkRequiredExtensions.remaining() + 1);
             extensions.put(vkRequiredExtensions);
-            extensions.put(stack.UTF8(KHRGetPhysicalDeviceProperties2.VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME));
+            extensions.put(stack.UTF8(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME));
             extensions.flip();
 
             VkInstanceCreateInfo vkInstanceCreateInfo = VkInstanceCreateInfo.create()
                     .ppEnabledExtensionNames(extensions)
-                    .pApplicationInfo(VkApplicationInfo.mallocStack(stack).apiVersion(VK11.VK_API_VERSION_1_1));
+                    .pApplicationInfo(VkApplicationInfo.mallocStack(stack).apiVersion(VK_API_VERSION_1_1));
 
 
             PointerBuffer vkInstanceBuffer = stack.mallocPointer(1);
-            if (VK11.vkCreateInstance(vkInstanceCreateInfo, null, vkInstanceBuffer) != VK10.VK_SUCCESS) {
-                GLFW.glfwTerminate();
+            if (vkCreateInstance(vkInstanceCreateInfo, null, vkInstanceBuffer) != VK_SUCCESS) {
                 throw new AssertionError("Failed to create VkInstance!");
             }
 
             VkInstance vkInstance = new VkInstance(vkInstanceBuffer.get(0), vkInstanceCreateInfo);
             LongBuffer surfaceBuffer = stack.mallocLong(1);
 
-            if (GLFWVulkan.glfwCreateWindowSurface(vkInstance, window, null, surfaceBuffer) != VK10.VK_SUCCESS) {
-                GLFW.glfwTerminate();
+            if (glfwCreateWindowSurface(vkInstance, window, null, surfaceBuffer) != VK_SUCCESS) {
                 throw new AssertionError("Failed to create Vulkan surface from window!");
             }
-            while (!GLFW.glfwWindowShouldClose(window)) {
 
-                GLFW.glfwPollEvents();
+            IntBuffer physicalDeviceCount = stack.mallocInt(1);
+            if (vkEnumeratePhysicalDevices(vkInstance, physicalDeviceCount, null) != VK_SUCCESS) {
+                throw new AssertionError("No physical devices!");
             }
+
+            System.out.println(physicalDeviceCount.get(0));
+
+            while (!glfwWindowShouldClose(window)) {
+
+                glfwPollEvents();
+            }
+        } finally {
+            glfwTerminate();
         }
-
-
-        GLFW.glfwTerminate();
     }
 }
