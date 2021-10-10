6e509a9bf2a081c768730ff401dfe3a114375c46
Aivech <16143392+Aivech@users.noreply.github.com>
2021-10-09 22:18:42 -0400

    Selecting a GPU

--- src/main/java/com/atomikmc/atomikvk/vulkan/Vulkan.java
+++ src/main/java/com/atomikmc/atomikvk/vulkan/Vulkan.java
@@ -5,20 +5,21 @@ import com.atomikmc.atomikvk.common.GraphicsProvider;
 import org.lwjgl.PointerBuffer;
 import org.lwjgl.glfw.GLFWVulkan;
 import org.lwjgl.system.MemoryStack;
-import org.lwjgl.system.MemoryUtil;
 import org.lwjgl.vulkan.*;
 
 import java.nio.IntBuffer;
 import java.nio.LongBuffer;
+import java.util.ArrayList;
+import java.util.Queue;
 
 import static org.lwjgl.vulkan.EXTDebugReport.VK_ERROR_VALIDATION_FAILED_EXT;
+import static org.lwjgl.vulkan.EXTDebugUtils.*;
 import static org.lwjgl.vulkan.KHRDisplaySwapchain.VK_ERROR_INCOMPATIBLE_DISPLAY_KHR;
 import static org.lwjgl.vulkan.KHRSurface.VK_ERROR_NATIVE_WINDOW_IN_USE_KHR;
 import static org.lwjgl.vulkan.KHRSurface.VK_ERROR_SURFACE_LOST_KHR;
 import static org.lwjgl.vulkan.KHRSwapchain.VK_ERROR_OUT_OF_DATE_KHR;
 import static org.lwjgl.vulkan.KHRSwapchain.VK_SUBOPTIMAL_KHR;
 import static org.lwjgl.vulkan.VK10.*;
-import static org.lwjgl.vulkan.EXTDebugUtils.*;
 
 
 public class Vulkan implements GraphicsProvider {
@@ -28,11 +29,13 @@ public class Vulkan implements GraphicsProvider {
 
     private VkInstance instance;
     private long vkDebugUtilsMessenger;
+    private VkPhysicalDevice gpu;
 
     @Override
     public void init() {
         createInstance();
         setupDebugMessenger();
+        chooseGPU();
     }
 
     @Override
@@ -42,18 +45,18 @@ public class Vulkan implements GraphicsProvider {
 
     @Override
     public void cleanup() {
-        EXTDebugUtils.vkDestroyDebugUtilsMessengerEXT(instance,vkDebugUtilsMessenger,null);
+        EXTDebugUtils.vkDestroyDebugUtilsMessengerEXT(instance, vkDebugUtilsMessenger, null);
         vkDestroyInstance(instance, null);
     }
 
     private void createInstance() {
-        try(MemoryStack stack = MemoryStack.stackPush()) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
             VkApplicationInfo pAppInfo = VkApplicationInfo.callocStack(stack);
             pAppInfo.sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
                     // .pApplicationName("AtomikVk")
-                    .applicationVersion(VK_MAKE_VERSION(1,0,0))
+                    .applicationVersion(VK_MAKE_VERSION(1, 0, 0))
                     // .pEngineName("No Engine")
-                    .engineVersion(VK_MAKE_VERSION(1,0,0))
+                    .engineVersion(VK_MAKE_VERSION(1, 0, 0))
                     .apiVersion(VK_API_VERSION_1_0);
 
             VkInstanceCreateInfo pCreateInfo = VkInstanceCreateInfo.callocStack(stack);
@@ -62,25 +65,25 @@ public class Vulkan implements GraphicsProvider {
 
             // get available extensions
             IntBuffer pPropertyCount = stack.mallocInt(1);
-            vkEnumerateInstanceExtensionProperties((CharSequence) null,pPropertyCount,null);
-            VkExtensionProperties.Buffer extensions = VkExtensionProperties.mallocStack(pPropertyCount.get(0),stack);
-            vkEnumerateInstanceExtensionProperties((CharSequence) null,pPropertyCount,extensions);
-            for(VkExtensionProperties ext: extensions) {
+            vkEnumerateInstanceExtensionProperties((CharSequence) null, pPropertyCount, null);
+            VkExtensionProperties.Buffer extensions = VkExtensionProperties.mallocStack(pPropertyCount.get(0), stack);
+            vkEnumerateInstanceExtensionProperties((CharSequence) null, pPropertyCount, extensions);
+            for (VkExtensionProperties ext : extensions) {
                 AtomikVk.LOGGER.debug(ext.extensionNameString());
             }
 
             // get required VK extensions
             PointerBuffer pGLFWVkRequiredExtensions = GLFWVulkan.glfwGetRequiredInstanceExtensions();
-            if(ENABLE_VALIDATION) {
+            if (ENABLE_VALIDATION) {
                 PointerBuffer pVkRequiredExtensions = stack.mallocPointer(
                         (pGLFWVkRequiredExtensions != null ? pGLFWVkRequiredExtensions.capacity() : 0) + debugExtensions.length);
                 if (pGLFWVkRequiredExtensions != null) {
-                    for(int i = 0; i < pGLFWVkRequiredExtensions.capacity(); i++) {
+                    for (int i = 0; i < pGLFWVkRequiredExtensions.capacity(); i++) {
                         pVkRequiredExtensions.put(pGLFWVkRequiredExtensions.get(i));
                     }
                 }
-                for(CharSequence str: debugExtensions) {
-                    pVkRequiredExtensions.put(stack.UTF8(str,true));
+                for (CharSequence str : debugExtensions) {
+                    pVkRequiredExtensions.put(stack.UTF8(str, true));
                 }
                 pVkRequiredExtensions.rewind(); // necessary or nothing gets loaded
 
@@ -90,12 +93,12 @@ public class Vulkan implements GraphicsProvider {
             }
 
             // check for validation layers
-            if(ENABLE_VALIDATION && !checkValidationLayerSupport()) {
+            if (ENABLE_VALIDATION && !checkValidationLayerSupport()) {
                 throw new AssertionError("Requested validation layers not present!");
             }
             // and enable them
             PointerBuffer pVkRequiredLayers = stack.mallocPointer(validationLayers.length);
-            for(CharSequence layer: validationLayers) {
+            for (CharSequence layer : validationLayers) {
                 pVkRequiredLayers.put(stack.UTF8(layer, true));
             }
             pVkRequiredLayers.rewind();  // necessary or nothing gets loaded
@@ -103,14 +106,14 @@ public class Vulkan implements GraphicsProvider {
 
             // create VkInstance
             PointerBuffer vkInstanceBuffer = stack.callocPointer(1);
-            _CHECK_(vkCreateInstance(pCreateInfo,null,vkInstanceBuffer), "Failed to create VkInstance!");
+            _CHECK_(vkCreateInstance(pCreateInfo, null, vkInstanceBuffer), "Failed to create VkInstance!");
             instance = new VkInstance(vkInstanceBuffer.get(0), pCreateInfo);
         }
     }
 
     private void setupDebugMessenger() {
         if (!ENABLE_VALIDATION) return;
-        try(MemoryStack stack = MemoryStack.stackPush()) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
             VkDebugUtilsMessengerCreateInfoEXT messengerCreateInfo = VkDebugUtilsMessengerCreateInfoEXT.callocStack(stack);
             messengerCreateInfo.sType(VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT)
                     .messageSeverity(VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
@@ -122,42 +125,116 @@ public class Vulkan implements GraphicsProvider {
                     .pfnUserCallback(Vulkan::VkDebugMessengerCallback);
 
             LongBuffer pVkDebugUtilsMessenger = stack.callocLong(1);
-            _CHECK_(EXTDebugUtils.vkCreateDebugUtilsMessengerEXT(instance,messengerCreateInfo,null,pVkDebugUtilsMessenger),"Failed to set up debug messenger!");
+            _CHECK_(EXTDebugUtils.vkCreateDebugUtilsMessengerEXT(instance, messengerCreateInfo, null, pVkDebugUtilsMessenger), "Failed to set up debug messenger!");
             vkDebugUtilsMessenger = pVkDebugUtilsMessenger.get(0);
+        }
+    }
+
+    private void chooseGPU() {
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer pPhysDeviceCount = stack.mallocInt(1);
+            vkEnumeratePhysicalDevices(instance,pPhysDeviceCount,null);
+            int physDeviceCount = pPhysDeviceCount.get(0);
+            if (physDeviceCount == 0) throw(new RuntimeException("No Vulkan-compatible devices!"));
+
+            PointerBuffer pPhysicalDevices = stack.mallocPointer(physDeviceCount);
+            vkEnumeratePhysicalDevices(instance, pPhysDeviceCount, pPhysicalDevices);
+            ArrayList<VkPhysicalDevice> devices = new ArrayList<>();
+            for(int i = 0; i < physDeviceCount; i++) {
+                devices.add(new VkPhysicalDevice(pPhysicalDevices.get(i), instance));
+            }
+
+            int score = 0;
+            for(VkPhysicalDevice device : devices) {
+                int deviceScore = evaluateDevice(device);
+                if (deviceScore > score) {
+                    score = deviceScore;
+                    gpu = device;
+                }
+            }
+
+            if (gpu == null) {
+                throw new RuntimeException("Failed to find a suitable GPU!");
+            }
+
         }
     }
 
     public static int VkDebugMessengerCallback(int messageSeverity, int messageTypes, long pCallbackData, long pUserData) {
         VkDebugUtilsMessengerCallbackDataEXT data = VkDebugUtilsMessengerCallbackDataEXT.create(pCallbackData);
-        AtomikVk.LOGGER.error("VK DEBUG: "+ data.pMessageString());
+        AtomikVk.LOGGER.error("VK DEBUG: " + data.pMessageString());
 
         return VK_FALSE;
     }
 
     private boolean checkValidationLayerSupport() {
-        try(MemoryStack stack = MemoryStack.stackPush()) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
             // get layer count
             IntBuffer pLayerCount = stack.mallocInt(1);
-            vkEnumerateInstanceLayerProperties(pLayerCount,null);
+            vkEnumerateInstanceLayerProperties(pLayerCount, null);
 
-            VkLayerProperties.Buffer pVkLayerProperties = VkLayerProperties.callocStack(pLayerCount.get(0),stack);
+            VkLayerProperties.Buffer pVkLayerProperties = VkLayerProperties.callocStack(pLayerCount.get(0), stack);
             vkEnumerateInstanceLayerProperties(pLayerCount, pVkLayerProperties);
 
             AtomikVk.LOGGER.debug("VK validation layers available:");
-            for(VkLayerProperties properties: pVkLayerProperties) {
+            for (VkLayerProperties properties : pVkLayerProperties) {
                 AtomikVk.LOGGER.debug(properties.layerNameString());
             }
 
             int count = 0;
-            for(CharSequence layerName : validationLayers) {
-                for(VkLayerProperties prop: pVkLayerProperties) {
-                    if (prop.layerNameString().contentEquals(layerName)){
+            for (CharSequence layerName : validationLayers) {
+                for (VkLayerProperties prop : pVkLayerProperties) {
+                    if (prop.layerNameString().contentEquals(layerName)) {
                         count++;
                         break;
                     }
                 }
             }
-            return count==validationLayers.length;
+            return count == validationLayers.length;
+        }
+    }
+
+    private int evaluateDevice(VkPhysicalDevice device) {
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            int score = 1;
+
+            VkPhysicalDeviceProperties properties = VkPhysicalDeviceProperties.callocStack(stack);
+            vkGetPhysicalDeviceProperties(device, properties);
+            VkPhysicalDeviceFeatures features = VkPhysicalDeviceFeatures.callocStack(stack);
+            vkGetPhysicalDeviceFeatures(device, features);
+
+            // get device queue families
+            findQueueFamilies(device);
+
+            // mandatory features
+            if(!features.geometryShader() || QueueFamilies.graphicsFamily == null) {
+                return -1; // a score of -1 will never be chosen, indicating an unsuitable device
+            }
+
+            // optional features
+            if (properties.deviceType() == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+                score += 100;
+
+
+            // TODO: add actual requirements - for now, nearly anything will do
+
+            AtomikVk.LOGGER.debug("Found candidate device "+properties.deviceNameString()+" with score = "+score);
+            return score;
+        }
+    }
+
+    private void findQueueFamilies(VkPhysicalDevice device) {
+        try(MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer pQueueFamilyCount = stack.mallocInt(1);
+            vkGetPhysicalDeviceQueueFamilyProperties(device,pQueueFamilyCount,null);
+            VkQueueFamilyProperties.Buffer queueFamilies = VkQueueFamilyProperties.callocStack(pQueueFamilyCount.get(0),stack);
+            vkGetPhysicalDeviceQueueFamilyProperties(device,pQueueFamilyCount,queueFamilies);
+
+            for(VkQueueFamilyProperties family : queueFamilies) {
+                if((family.queueFlags() & VK_QUEUE_GRAPHICS_BIT) != 0) {
+                    QueueFamilies.graphicsFamily = family;
+                }
+            }
         }
     }
 
@@ -198,5 +275,11 @@ public class Vulkan implements GraphicsProvider {
             case VK_ERROR_VALIDATION_FAILED_EXT -> "A validation layer found an error.";
             default -> String.format("%s [%d]", "Unknown", result);
         };
+    }
+
+    // java structs when
+    private class QueueFamilies {
+        private static VkQueueFamilyProperties graphicsFamily;
+
     }
 }
